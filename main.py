7645ac5dfcfb9fe13bad5a58b9b42e8acd877f56
Aimee Gonell <aimee.gonell@gmail.com>
2021-03-27 18:18:37 +0100

    discover whether or not it is running in the main scope

--- main.py
+++ main.py
@@ -23,39 +23,45 @@ the two wins, or there is a tie (after 9 turns).
 
 import utils
 
-# Build the game board
-game_board = [
-    ["-|", "-|", "-"],
-    ["-|", "-|", "-"],
-    ["-|", "-|", "-"]
-]
-
-# when player is True, it refers to 'x', on the contrary, it means 'o'
-player = True
-
-# Variable to count the turns
-turns = 0
-
 # Game Loop for a single game of Tic Tac Toe
-while turns >= 0 and turns < 9:
-    active_player = utils.current_player((player))
-    utils.display_board(game_board)
-    player_input = input("Enter a position 1 through 9, or enter \"e\" to exit game: ") 
-    if utils.exit_game(player_input):
-        break
-    if not utils.check_play(player_input):
-        print("Please try again")
-        continue
-    player_input = int(player_input) - 1
-    coordinates = utils.position(player_input)
-    if utils.is_taken(coordinates, game_board):
-        print("Please choose another position")
-        continue
-    utils.add_position(coordinates, game_board, active_player)
-    if utils.win_game(active_player, game_board):
-        print(f"{active_player.upper()} won!")
-        break
-    turns += 1
-    if turns == 9:
-        print("It's a Tie!")
-    player = not player
+def main():
+    # Build the game board
+    game_board = [
+        ["-|", "-|", "-"],
+        ["-|", "-|", "-"],
+        ["-|", "-|", "-"]
+    ]
+
+    # when player is True, it refers to 'x', on the contrary, it means 'o'
+    player = True
+
+    # Variable to count the turns
+    turns = 0
+
+    while turns >= 0 and turns < 9:
+        active_player = utils.current_player((player))
+        utils.display_board(game_board)
+        player_input = input("Enter a position 1 through 9, or enter \"e\" to exit game: ") 
+        if utils.exit_game(player_input):
+            break
+        if not utils.check_play(player_input):
+            print("Please try again")
+            continue
+        player_input = int(player_input) - 1
+        coordinates = utils.position(player_input)
+        if utils.is_taken(coordinates, game_board):
+            print("Please choose another position")
+            continue
+        utils.add_position(coordinates, game_board, active_player)
+        if utils.win_game(active_player, game_board):
+            print(f"{active_player.upper()} won!")
+            break
+        turns += 1
+        if turns == 9:
+            print("It's a Tie!")
+        player = not player
+
+
+if __name__ == "__main__":
+    # execute only if run as a script
+    main()
